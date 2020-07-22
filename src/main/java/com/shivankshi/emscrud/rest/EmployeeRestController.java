e227e9482fb32950587cd039fe4fa5570fe1c4b7
Shivankshikh <shivankshikh@gmail.com>
2020-07-22 14:16:59 +0530

    updateEmployee method is updated by using RequestBody parameter

--- src/main/java/com/shivankshi/emscrud/rest/EmployeeRestController.java
+++ src/main/java/com/shivankshi/emscrud/rest/EmployeeRestController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+//import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shivankshi.emscrud.service.EmployeeService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 
 import com.shivankshi.emscrud.entity.Employee;
 
@@ -29,9 +32,10 @@ public class EmployeeRestController {
 	public EmployeeRestController(EmployeeService employeeService) {
 		this.employeeService = employeeService;
 	}
+	//@ApiOperation(value = "", authorizations = { @Authorization(value = "BasicAuth") })
 	
 	@GetMapping("/employees")
-	
+	@ApiOperation(value ="Get List of Existing Employees",authorizations = {@Authorization(value="BasicAuth")} )
 	public List<Employee> getEmployees()
 	{
 			return employeeService.getEmployees();
@@ -39,6 +43,7 @@ public class EmployeeRestController {
 	
 	
 	@GetMapping("/employees/{empId}")
+	@ApiOperation(value ="Get Employee by id", notes = "Provide an id to look up specific Employee from the Database", response = Employee.class)
 	public Employee getEmployee(@PathVariable int empId)
 	{
 		Employee theEmployee =employeeService.findById(empId);
@@ -50,6 +55,7 @@ public class EmployeeRestController {
 	}
 	
 	@PostMapping("/employees")
+	@ApiOperation(value ="Add a new Employee", notes = "Provide an Employee", response = Employee.class, authorizations = {@Authorization(value="BasicAuth")})
 	public Employee addEmployee(@RequestBody Employee theEmployee)
 	{
 		//RequestBody for binding json data with Employee object
@@ -66,6 +72,7 @@ public class EmployeeRestController {
 	
 	
 	@PutMapping("/employees/{empId}")
+	@ApiOperation(value ="Update Existing Employee", notes = "Only salary or designation can be updated", response = Employee.class ,authorizations = {@Authorization(value="BasicAuth")})
 	public Employee updateEmployee(@PathVariable("empId") int empId, @RequestBody Employee theEmployee)
 	{
 		
@@ -73,6 +80,7 @@ public class EmployeeRestController {
 	}
 	
 	@DeleteMapping("/employees/{empId}")
+	@ApiOperation(value ="Delete an Existing Employee", notes = "Provide an employee Id to be deleted" ,authorizations = {@Authorization(value="BasicAuth")})
 	public String deleteEmployee(@PathVariable int empId)
 	{
 		Employee tEmployee= employeeService.findById(empId);
