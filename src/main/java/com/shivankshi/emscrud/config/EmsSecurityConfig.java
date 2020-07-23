3c4cb8a4067497e6cf31d09e2cbf9475482253b2
Shivankshikh <shivankshikh@gmail.com>
2020-07-23 19:08:25 +0530

    OAuthSecurity Enabled

--- src/main/java/com/shivankshi/emscrud/config/EmsSecurityConfig.java
+++ src/main/java/com/shivankshi/emscrud/config/EmsSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.shivankshi.emscrud.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,8 +8,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-
+@EnableOAuth2Sso
 public class EmsSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+        .csrf()
+        .disable()
+        .antMatcher("/**")
+        .authorizeRequests()
+        .antMatchers("/")
+        .permitAll()
+        .anyRequest()
+        .authenticated();
+	}
+	
+	
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -19,23 +35,23 @@ public class EmsSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		
-		
-
-		
-		http.httpBasic()
-		.and()
-		.authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/api/employees/**").hasRole("USER")
-		.antMatchers(HttpMethod.POST, "/api/employees").hasRole("ADMIN")
-		.antMatchers(HttpMethod.PUT,"/api/employees/**").hasRole("ADMIN")
-		.antMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
-		.and()
-		.csrf().disable()
-		.formLogin().disable();
-	}
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		
+//		
+//
+//		
+//		http.httpBasic()
+//		.and()
+//		.authorizeRequests()
+//		.antMatchers(HttpMethod.GET, "/api/employees/**").hasRole("USER")
+//		.antMatchers(HttpMethod.POST, "/api/employees").hasRole("ADMIN")
+//		.antMatchers(HttpMethod.PUT,"/api/employees/**").hasRole("ADMIN")
+//		.antMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+//		.and()
+//		.csrf().disable()
+//		.formLogin().disable();
+//	}
 	
 	
 
