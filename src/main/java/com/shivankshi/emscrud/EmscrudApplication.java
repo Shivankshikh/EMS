c0f07862907b29ea86117b1a20b90f849b93a1e1
Shivankshikh <shivankshikh@gmail.com>
2020-07-29 17:34:37 +0530

    Unnecessary lines deleted.

--- src/main/java/com/shivankshi/emscrud/EmscrudApplication.java
+++ src/main/java/com/shivankshi/emscrud/EmscrudApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmscrudApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmscrudApplication.class, args);
 	}
