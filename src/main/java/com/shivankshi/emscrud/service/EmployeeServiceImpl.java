ada863a6f9a888235d68734222c920e21e861c95
Shivankshikh <shivankshikh@gmail.com>
2020-07-29 17:32:59 +0530

    formatted EmployeeServiceImpl

--- src/main/java/com/shivankshi/emscrud/service/EmployeeServiceImpl.java
+++ src/main/java/com/shivankshi/emscrud/service/EmployeeServiceImpl.java
@@ -4,86 +4,65 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
-
 
 import com.shivankshi.emscrud.dao.EmployeeRepository;
 import com.shivankshi.emscrud.entity.Employee;
-
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
 	private EmployeeRepository employeeRepository;
-	
+
 	@Autowired
 	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
-		//bean id due to conflict in DAO implementation
+
 		this.employeeRepository = employeeRepository;
 	}
-	
-	
-	
-	//following service methods will delegate the calls to the DAO
 
 	@Override
 	public List<Employee> getEmployees() {
-		
+
 		return employeeRepository.findAll();
 	}
 
 	@Override
 	public Employee findById(int theId) {
-		// TODO Auto-generated method stub
+
 		Optional<Employee> result = employeeRepository.findById(theId);
-		Employee employee=null;
-		if(result.isPresent())
-		{
-		employee=result.get();	
-		}
-		else {
+		Employee employee = null;
+		if (result.isPresent()) {
+			employee = result.get();
+		} else {
 			throw new RuntimeException("Employee id not found");
 		}
 		return employee;
 	}
-
-//	@Override
-//	public Employee save(Employee theEmployee) {
-//		return employeeRepository.save(theEmployee);
-//
-//	}
 
 	@Override
 	public void deleteById(int theId) {
 		employeeRepository.deleteById(theId);
 	}
 
+	@Override
+	public Employee addEmployee(Employee employee) {
 
-
-@Override
-public Employee addEmployee(Employee employee) {
-	
-	return employeeRepository.save(employee);
-}
-
-
-
-@Override
-public Employee updateEmployee(int empId, int salary, String designation) {
-	Optional<Employee> employeeFromDb=employeeRepository.findById(empId);
-	Employee employee=null;
-	if(employeeFromDb.isPresent())
-	{
-		employee=employeeFromDb.get();
-		employee.setSalary(salary);
-		employee.setDesignation(designation);
+		return employeeRepository.save(employee);
 	}
-	else {
-		throw new RuntimeException("Employee id not found");
+
+	@Override
+	public Employee updateEmployee(int empId, Employee employee) {
+		Optional<Employee> employeeFromDb = employeeRepository.findById(empId);
+		Employee employee2 = null;
+		if (employeeFromDb.isPresent()) {
+			employee2 = employeeFromDb.get();
+			employee2.setDesignation(employee.getDesignation());
+			employee2.setSalary(employee.getSalary());
+		} else {
+			throw new RuntimeException("Employee id not found");
+		}
+		employeeRepository.save(employee2);
+		return employee2;
 	}
-	
-	return employee;
-}
 
 }
